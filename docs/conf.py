76c67b271f339bed10976b55a7183a80aedc5b54
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-07-31 15:23:49 -0300

    Bump to 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ author = "Lucas Rangel Cezimbra"
 # the built documents.
 #
 # The short X.Y version.
-version = '1.0.3'
+version = '1.1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
