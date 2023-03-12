a2dabb030779e4eddc286761875b33c1d497a81d
davibobsin <39809322+davibobsin@users.noreply.github.com>
2023-03-12 14:57:03 -0300

    Add support for Monthly Statements (#201)
    * chore: update pre-commit-config
    
    * feat: add monthly statement getter
    
    * chore: update tests

--- pyitau/pages.py
+++ pyitau/pages.py
@@ -183,9 +183,16 @@ class CardsPage(SoupPage):
 
 class CheckingAccountFullStatement(TextPage):
     @property
-    def filter_statements_op(self):
+    def filter_statements_by_period_op(self):
         pattern = 'function consultarLancamentosPorPeriodo.*' \
-                  '"periodoConsulta" : parametrosPeriodo.*' \
+                  '"periodoConsulta" : parametrosPeriodo.*?' \
+                  'url = "(.*?)";'
+        return re.search(pattern, self._text, flags=re.DOTALL).group(1)
+
+    @property
+    def filter_statements_by_month_op(self):
+        pattern = 'function consultarLancamentosPorPeriodo.*' \
+                  '"mesCompleto" : parametrosPeriodo.*?' \
                   'url = "(.*?)";'
         return re.search(pattern, self._text, flags=re.DOTALL).group(1)
 
