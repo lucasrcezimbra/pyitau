d27c24a2389ae0eae952ae34bce64bf3bc21c479
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-08-21 09:43:10 -0300

    Add test fo AuthenticatedHome.menu_op

--- tests/pages/test_authenticated_home.py
+++ tests/pages/test_authenticated_home.py
@@ -7,20 +7,49 @@ from pyitau.pages import AuthenticatedHomePage
 @pytest.fixture
 def response():
     return """
+<input type="hidden" id="portalTxt" value="varejo"/>
+<script id="item" type="text/javascript">
+    var obterMenu = function() {
+        var perfil = $("#portalTxt").val();
+        $.ajax({
+            url : "PYITAU_MENU_OP",
+            dataType : "html",
+            method : "POST",
+            async: true,
+            showLoading: false,
+            headers : {
+                "ajaxRequest" : true
+            },
+            success : function(data) {
+                $("#menu_p_fisica").replaceWith(data);
+
+            },
+            error : function(erro){
+                $("#formError").submit();
+            }
+        });
+    };
+
+    if(window.addEventListener){
+        window.addEventListener('load', obterMenu)
+    } else {
+        window.attachEvent('onload', obterMenu)
+    }
+</script></li>
+            </ul>
+        </nav>
+
         <div class="logo left">
-            <a
-               data-op="PYITAU_OP"
-               href=""
-               id="HomeLogo"
-               onclick="GA.pushHeader('logoItau');"
-               title="Home"
-            >
-                <img
-                    alt="Logo Itaú"
-                    height="50"
-                    src="https://estatico.itau.com.br/.../logo-itau.png"
-                    width="50"
-                />
+            <a onclick="GA.pushHeader('logoItau');" href=""
+                title="Home"
+                id="HomeLogo"
+                data-op="PYITAU_OP">
+
+
+                    <img src="https://estatico.itau.com.br/.../logo-itau.png" width="50"
+                            height="50" alt="Logo Ita&uacute;" />
+
+
             </a>
         </div>
     """
@@ -34,6 +63,10 @@ def page(response):
 def test_init(response):
     page = AuthenticatedHomePage(response)
     assert page._soup == BeautifulSoup(response, features='html.parser')
+
+
+def test_menu_op(page):
+    assert page.menu_op == 'PYITAU_MENU_OP'
 
 
 def test_op(page):
